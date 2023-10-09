c640adef14a56bb3f9770c886d22d32a5b2e8db6
Eldana <fineshine6718@gmail.com>
2023-10-09 23:18:11 +0600

    header/scss

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import s from '../styles/header.module.scss'
+import logo1 from '../assets/logo1.svg'
+import logo2 from '../assets/logo2.svg'
 
 const Header = () => {
   return (
     <>
-    <Link to='/'>
+    <header>
+    <div className={s.images}>
+      <Link to='/'>
+         <img src={logo1} alt="" />
+    <img src={logo2} alt="" />
+      </Link>
+    </div>
+    <div className={s.container}>
+    <Link to='/' className={s.home}>
         Главная
     </Link>
-    <Link to='/range'>
+    <Link to='/range' className={s.home}>
         Ассортимент
     </Link>
-    <Link to='/contacts'>
+    <Link to='/contacts' className={s.home}>
         Контакты
     </Link>
-    <Link to='/about-us'>
+    <Link to='/about-us' className={s.home}>
         О нас
     </Link>
+    </div>
+    </header>
+   
     </>
   )
 }
